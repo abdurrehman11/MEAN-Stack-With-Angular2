a66b6a5a0671a7ad0f15353d175a2acd0f999308
Abdur Rehman <abdurrehman245@gmail.com>
2017-07-31 21:22:57 +0500

    user-model

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = express.Router();
 const config = require('./config/database');   
-const path = require('path');     
+const path = require('path');  
+const authentication = require('./routes/authentication')(router);  
+const bodyParser = require('body-parser'); 
 
 const app = express();
 
@@ -15,10 +18,20 @@ mongoose.createConnection(config.uri, (err) => {
     }
 });
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Provide static directory for frontend
 app.use(express.static(__dirname + '/client/dist/'));
 
+app.use('/authentication', authentication);
+
+// Connect server to Angular2 index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
 app.listen(3000, () => {
